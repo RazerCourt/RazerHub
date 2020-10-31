7a3d3399588043781e7e9a530ce6a98fefa56da5
RazerCourt <73623743+RazerCourt@users.noreply.github.com>
2020-10-31 12:28:16 -0400

    Update hdnnqdwwndwqdqWLMANAGER.php

--- hdnnqdwwndwqdqWLMANAGER.php
+++ hdnnqdwwndwqdqWLMANAGER.php
@@ -1,10 +1,11 @@
 <?php
 $a = $_GET["SxfKey"];
-$b = $_GET["key"]
+$b = $_GET["key"];
 if(!isset($a)) {
 return; 
 }
 if ($a === "9081DelndjadnadsadaLODRAMAdhwdlavodramadnajdaudy3g32sandrine") {
+  echo "hi..."
   if(!isset($key)) {
    return; 
   }
