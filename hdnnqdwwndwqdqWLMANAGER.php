378ccb355a21fc6af21aff4e5964307418554906
RazerCourt <73623743+RazerCourt@users.noreply.github.com>
2020-10-31 12:24:03 -0400

    Update and rename index.php to hdnnqdwwndwqdqWLMANAGER.php

---
+++ hdnnqdwwndwqdqWLMANAGER.php
@@ -0,0 +1,18 @@
+<?php
+$a = $_GET["SxfKey"];
+$b = $_GET["key"]
+if(!isset($a)) {
+return; 
+}
+if ($a === "9081DelndjadnadsadaLODRAMAdhwdlavodramadnajdaudy3g32sandrine") {
+  if(!isset($key)) {
+   return; 
+  }
+$fh = fopen('whitelisted.lua', 'a'); 
+fwrite($fh, $key."\n");
+}
+else
+{
+ return; 
+}
+?>
